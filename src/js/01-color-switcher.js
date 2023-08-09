e2ac16475f2830a9268327650947d32fb2921c5d
777Roma777 <nanoeagle911@gmail.com>
2023-08-09 15:21:33 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let interval = null;
+
+startButton.addEventListener('click', startChange);
+stopButton.addEventListener('click', stopChange);
+
+function backgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function startChange() {
+  startButton.disabled = true;
+ interval = setInterval(backgroundColor, 1000);
+}
+
+function stopChange() {
+    startButton.disabled = false;
+    clearInterval(interval);
+
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
