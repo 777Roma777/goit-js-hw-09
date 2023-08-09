84bddfa619c8a8091ed6f7ecc730091a577e5820
777Roma777 <nanoeagle911@gmail.com>
2023-08-09 17:13:14 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,35 @@
+import Notiflix from 'notiflix';
+
 const form = document.querySelector('.form');
 
 const delayInput = document.querySelector('input[name="delay"]');
 const stepInput = document.querySelector('input[name="step"]');
 const amountInput = document.querySelector('input[name="amount"]');
 
+form.addEventListener('submit', handleSubmit);
 
+function handleSubmit(e) {
+  e.preventDefault();
 
+  let initialDelay = Number(delayInput.value);
+  let step = Number(stepInput.value);
+  let amount = Number(amountInput.value);
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, initialDelay + step * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+  // form.reset();
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
